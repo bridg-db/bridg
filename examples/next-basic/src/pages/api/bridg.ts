cb9e15a2add767d7d181967a73f357cf403fcfaf
Joe Roddy <mrjoeroddy@gmail.com>
2023-02-19 14:58:27 -0500

    improve example, rm next files

--- examples/next-basic/src/pages/api/bridg.ts
+++ examples/next-basic/src/pages/api/bridg.ts
@@ -2,10 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 import db from 'prisma/db';
 import { handleRequest } from 'bridg/app/server/request-handler';
-import { dbRules } from 'prisma/db-rules';
+import { dbRules } from '@/pages';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  // Mock authentication
+  // Mock authentication, replace with any auth system you want.
   const userId = 'cld4ar9fg000clfd96gujbblu';
   const { data, status } = await handleRequest(req.body, { db, uid: userId, rules: dbRules });
 
--- examples/next-basic/src/pages/index.tsx
+++ examples/next-basic/src/pages/index.tsx
@@ -1,14 +1,22 @@
 import { NextPage } from 'next';
 import { useEffect, useState } from 'react';
 import db from 'bridg/app/client/db';
+import { DbRules } from 'bridg/app/server/request-handler';
 
-interface Props {}
-
-const Name: NextPage<Props> = ({}) => {
+const BridgExample: NextPage = ({}) => {
   const [data, setData] = useState({});
+
   useEffect(() => {
     (async () => {
-      const data = await db.blog.findMany({ include: { user: true } });
+      // Query your DB from the frontend 😎
+      const data = await db.user.findMany({
+        // filter your results
+        where: {
+          email: { contains: '@prisma.io' },
+        },
+        // include related data
+        include: { blogs: true },
+      });
 
       setData(data);
     })();
@@ -16,6 +24,22 @@ const Name: NextPage<Props> = ({}) => {
 
   return data === undefined ? <GetStarted /> : <pre>{JSON.stringify(data, null, 1)}</pre>;
 };
-export default Name;
+
+export const dbRules: DbRules = {
+  blog: {
+    get: true,
+    patch: (uid) => ({ userId: uid }),
+    post: (uid, data) => data?.title === 'This blog title is allowed',
+    delete: (uid) => ({ userId: uid }),
+  },
+  user: {
+    get: true,
+    patch: (uid) => ({ id: uid }),
+    post: true,
+    delete: false,
+  },
+};
+
+export default BridgExample;
 
 const GetStarted = () => <div>Fetch some data to get started</div>;
