5f5809415de2a2fdab50a1d3afe480430e3e41b1
Joe Roddy <mrjoeroddy@gmail.com>
2023-04-02 10:23:53 -0400

    where clause test coverage for all methods

--- src/playground/nextjs/tests/rules.test.ts
+++ src/playground/nextjs/tests/rules.test.ts
@@ -1,27 +1,28 @@
 import { Blog, PrismaClient } from '@prisma/client';
 import bridg from 'tests/bridg/client-db';
 import { setRules } from 'tests/bridg/test-rules';
-import { afterAll, beforeAll, beforeEach, expect, it } from 'vitest';
+import { afterAll, beforeAll, beforeEach, expect, it, test } from 'vitest';
 
 const prisma = new PrismaClient();
 
 const TEST_TITLE = 'TEST_BLOG';
 const TEST_TITLE_2 = 'TEST_BLOG_2';
-let testBlog: Blog;
+let testBlog1: Blog;
 let testBlog2: Blog;
 
-beforeAll(async () => {
+beforeEach(async () => {
+  setRules({});
   await prisma.blog.deleteMany();
-  testBlog = await prisma.blog.create({ data: { title: TEST_TITLE } });
+  testBlog1 = await prisma.blog.create({ data: { title: TEST_TITLE } });
   testBlog2 = await prisma.blog.create({ data: { title: TEST_TITLE_2 } });
 });
 
 afterAll(async () => {
   setRules({});
-  // await prisma.blog.deleteMany({ where: { id: { in: [testBlog.id, testBlog2.id] } } });
 });
 
-beforeEach(() => setRules({}));
+const fetchBlog1 = () => prisma.blog.findFirst({ where: { id: testBlog1.id } });
+const fetchBlog2 = () => prisma.blog.findFirst({ where: { id: testBlog2.id } });
 
 const queryFails = async (query: Promise<any>) => {
   const data = await query.catch((err) => {
@@ -32,60 +33,66 @@ const queryFails = async (query: Promise<any>) => {
 
 const querySucceeds = async (query: Promise<any>, resultCount = 1) => {
   const data = await query;
-  if (resultCount !== undefined) {
-    if (Array.isArray(data)) {
-      expect(data.length).toBe(resultCount);
-    } else {
-      resultCount === 0 && expect(data).toBeNull();
-      resultCount === 1 && expect(data).toBeTruthy();
-      if (resultCount > 1) throw Error('Expected array, but received ', data);
-    }
+
+  if (Array.isArray(data)) {
+    expect(data.length).toBe(resultCount);
+  } else if (data?.count !== undefined) {
+    expect(data.count).toBe(resultCount);
+  } else {
+    resultCount === 0 && expect(data).toBeNull();
+    resultCount === 1 && expect(data).toBeTruthy();
+    if (resultCount > 1) throw Error('Expected array, but received ', data);
   }
+
   return data;
 };
 
 it('Find rules work with true/false', async () => {
+  // FAIL
   setRules({ blog: { find: false } });
   await queryFails(bridg.blog.findMany({ where: { title: TEST_TITLE } }));
   await queryFails(bridg.blog.findFirst({ where: { title: TEST_TITLE } }));
   await queryFails(bridg.blog.findFirstOrThrow({ where: { title: TEST_TITLE } }));
-  await queryFails(bridg.blog.findUnique({ where: { id: testBlog.id } }));
-  await queryFails(bridg.blog.findUniqueOrThrow({ where: { id: testBlog.id } }));
-  await queryFails(bridg.blog.aggregate({ where: { id: testBlog.id } }));
+  await queryFails(bridg.blog.findUnique({ where: { id: testBlog1.id } }));
+  await queryFails(bridg.blog.findUniqueOrThrow({ where: { id: testBlog1.id } }));
+  await queryFails(bridg.blog.aggregate({ where: { id: testBlog1.id } }));
   await queryFails(bridg.blog.count());
   await queryFails(bridg.blog.groupBy());
-  await queryFails(bridg.blog.aggregate({ where: { id: testBlog.id } }));
+  await queryFails(bridg.blog.aggregate({ where: { id: testBlog1.id } }));
 
+  // SUCCESS
   setRules({ blog: { find: true } });
   await querySucceeds(bridg.blog.findMany(), 2);
   await querySucceeds(bridg.blog.findFirst({ where: { title: TEST_TITLE } }));
   await querySucceeds(bridg.blog.findFirstOrThrow({ where: { title: TEST_TITLE } }));
-  await querySucceeds(bridg.blog.findUnique({ where: { id: testBlog.id } }));
-  await querySucceeds(bridg.blog.findUniqueOrThrow({ where: { id: testBlog.id } }));
+  await querySucceeds(bridg.blog.findUnique({ where: { id: testBlog1.id } }));
+  await querySucceeds(bridg.blog.findUniqueOrThrow({ where: { id: testBlog1.id } }));
   await querySucceeds(bridg.blog.count());
   // TODO:
   // await querySucceeds(bridg.blog.groupBy());
   // await querySucceeds(bridg.blog.aggregate({ where: { id: testBlog.id } }));
 });
 
-it('Find rules work with queries', async () => {
+it('Find rules work with where clauses', async () => {
+  // FAIL
   setRules({ blog: { find: { title: 'SOMETHING_THAT_ISNT_IN_THE_DB' } } });
   await querySucceeds(bridg.blog.findMany({ where: { title: TEST_TITLE } }), 0);
   await querySucceeds(bridg.blog.findFirst({ where: { title: TEST_TITLE } }), 0);
   await querySucceeds(bridg.blog.findFirst({ where: { title: TEST_TITLE } }), 0);
-  await querySucceeds(bridg.blog.findUnique({ where: { id: testBlog.id } }), 0);
+  await querySucceeds(bridg.blog.findUnique({ where: { id: testBlog1.id } }), 0);
   // throws error (fails), bc of method
   await queryFails(bridg.blog.findFirstOrThrow({ where: { title: TEST_TITLE } }));
-  await queryFails(bridg.blog.findUniqueOrThrow({ where: { id: testBlog.id } }));
+  await queryFails(bridg.blog.findUniqueOrThrow({ where: { id: testBlog1.id } }));
 
+  // SUCCESS
   setRules({ blog: { find: { title: TEST_TITLE } } });
   await querySucceeds(bridg.blog.findMany(), 1);
   await querySucceeds(bridg.blog.findMany({ where: { title: TEST_TITLE_2 } }), 0);
   await querySucceeds(bridg.blog.findFirst({ where: { title: TEST_TITLE } }));
   await querySucceeds(bridg.blog.findFirst({ where: { title: TEST_TITLE } }));
   await querySucceeds(bridg.blog.findFirstOrThrow({ where: { title: TEST_TITLE } }));
-  await querySucceeds(bridg.blog.findUnique({ where: { id: testBlog.id } }));
-  await querySucceeds(bridg.blog.findUniqueOrThrow({ where: { id: testBlog.id } }));
+  await querySucceeds(bridg.blog.findUnique({ where: { id: testBlog1.id } }));
+  await querySucceeds(bridg.blog.findUniqueOrThrow({ where: { id: testBlog1.id } }));
   await querySucceeds(bridg.blog.count());
   // TODO:
   // await querySucceeds(bridg.blog.aggregate({ where: { id: testBlog.id } }));
@@ -95,61 +102,87 @@ it('Find rules work with queries', async () => {
 const TEST_UPDATE_BODY = 'TEST_BLOG_UPDATED';
 
 it('Update rules work with true/false', async () => {
+  // FAIL
   setRules({ blog: { update: false } });
 
-  await queryFails(bridg.blog.update({ where: { id: testBlog.id }, data: { body: TEST_UPDATE_BODY } }));
+  await queryFails(bridg.blog.update({ where: { id: testBlog1.id }, data: { body: TEST_UPDATE_BODY } }));
   await queryFails(bridg.blog.updateMany({ data: { body: TEST_UPDATE_BODY } }));
 
-  let record = await prisma.blog.findFirst({ where: { id: testBlog.id } });
+  let record = await fetchBlog1();
   expect(record?.body).toBeNull(); // record wasn't updated
 
+  // SUCCESS
   setRules({ blog: { update: true } });
-  await querySucceeds(bridg.blog.update({ where: { id: testBlog.id }, data: { body: TEST_UPDATE_BODY } }));
-  await querySucceeds(bridg.blog.updateMany({ where: { id: testBlog.id }, data: { body: TEST_UPDATE_BODY } }));
+  await querySucceeds(bridg.blog.update({ where: { id: testBlog1.id }, data: { body: TEST_UPDATE_BODY } }));
+  await querySucceeds(bridg.blog.updateMany({ where: { id: testBlog1.id }, data: { body: TEST_UPDATE_BODY } }));
 
-  record = await prisma.blog.findFirst({ where: { id: testBlog.id } });
+  record = await fetchBlog1();
   expect(record?.body).toBe(TEST_UPDATE_BODY); // record updated
 });
 
-it('Update rules work with queries', async () => {
-  await prisma.blog.update({ where: { id: testBlog.id }, data: { body: null } });
-  setRules({ blog: { update: false } });
+it('Update rules work with where clauses', async () => {
+  // FAIL
+  await prisma.blog.update({ where: { id: testBlog1.id }, data: { body: null } });
+  setRules({ blog: { update: { title: 'TITLE_THAT_DOESNT_EXIST' } } });
 
-  await queryFails(bridg.blog.update({ where: { id: testBlog.id }, data: { body: TEST_UPDATE_BODY } }));
-  await queryFails(bridg.blog.updateMany({ data: { body: TEST_UPDATE_BODY } }));
+  // query throws bc update should find at least one and it doesnt find any
+  await queryFails(bridg.blog.update({ where: { id: testBlog1.id }, data: { body: TEST_UPDATE_BODY } }));
+  await querySucceeds(bridg.blog.updateMany({ data: { body: TEST_UPDATE_BODY } }), 0);
 
-  let record = await prisma.blog.findFirst({ where: { id: testBlog.id } });
-  expect(record?.body).toBeNull(); // record wasn't updated
+  expect((await fetchBlog1())?.body).toBeNull(); // record wasn't updated
 
-  setRules({ blog: { update: true } });
-  await querySucceeds(bridg.blog.update({ where: { id: testBlog.id }, data: { body: TEST_UPDATE_BODY } }));
-  await querySucceeds(bridg.blog.updateMany({ where: { id: testBlog.id }, data: { body: TEST_UPDATE_BODY } }));
+  // SUCCESS
+  setRules({ blog: { update: { title: TEST_TITLE } } });
+  await querySucceeds(bridg.blog.update({ where: { id: testBlog1.id }, data: { body: TEST_UPDATE_BODY } }));
+  await querySucceeds(bridg.blog.updateMany({ where: { id: testBlog1.id }, data: { body: TEST_UPDATE_BODY } }), 1);
 
-  record = await prisma.blog.findFirst({ where: { id: testBlog.id } });
-  expect(record?.body).toBe(TEST_UPDATE_BODY); // record updated
+  expect((await fetchBlog1())?.body).toBe(TEST_UPDATE_BODY); // record updated
+  expect((await fetchBlog2())?.body).toBeNull(); // not updated bc didn't fullfill where clause
 });
 
 it('Create rules work with true/false', async () => {
+  // FAIL
   setRules({ blog: { create: false } });
   await queryFails(bridg.blog.create({ data: { title: TEST_TITLE } }));
   // TODO: createMany not supported via SQLite
   // await queryFails(bridg.blog.createMany({}));
 
+  // SUCCESS
   setRules({ blog: { create: true } });
   const b1 = await querySucceeds(bridg.blog.create({ data: { title: TEST_TITLE } }));
   expect(b1?.title).toBe(TEST_TITLE);
 });
 
 it('Delete rules work with true/false', async () => {
+  // FAIL
   setRules({ blog: { delete: false } });
-  await queryFails(bridg.blog.delete({ where: { id: testBlog.id } }));
-  await queryFails(bridg.blog.deleteMany({ where: { id: testBlog.id } }));
+  await queryFails(bridg.blog.delete({ where: { id: testBlog1.id } }));
+  await queryFails(bridg.blog.deleteMany({ where: { id: testBlog1.id } }));
 
+  // SUCCESS
   setRules({ blog: { delete: true } });
-  await querySucceeds(bridg.blog.delete({ where: { id: testBlog.id } }));
-  testBlog = await prisma.blog.create({ data: { title: TEST_TITLE } });
-  await querySucceeds(bridg.blog.deleteMany({ where: { id: testBlog.id } }));
+  await querySucceeds(bridg.blog.delete({ where: { id: testBlog1.id } }));
+  testBlog1 = await prisma.blog.create({ data: { title: TEST_TITLE } });
+  await querySucceeds(bridg.blog.deleteMany({ where: { id: testBlog1.id } }));
 
-  const record = await prisma.blog.findUnique({ where: { id: testBlog.id } });
-  expect(record).toBeNull(); // record was deleted
+  expect(await fetchBlog1()).toBeNull(); // record was deleted
+});
+
+it('Delete rules work with where clauses', async () => {
+  // FAIL
+  setRules({ blog: { delete: { title: 'TITLE_THAT_DOESNT_EXIST' } } });
+  await queryFails(bridg.blog.delete({ where: { id: testBlog1.id } }));
+  await querySucceeds(bridg.blog.deleteMany({ where: { id: testBlog1.id } }), 0);
+
+  // SUCCESS
+  setRules({ blog: { delete: { title: TEST_TITLE } } });
+  await querySucceeds(bridg.blog.delete({ where: { id: testBlog1.id } }));
+  expect(await fetchBlog1()).toBeNull();
+  expect(await fetchBlog2()).toBeTruthy();
+
+  testBlog1 = await prisma.blog.create({ data: { title: TEST_TITLE } });
+  expect(await fetchBlog1()).toBeTruthy();
+  await querySucceeds(bridg.blog.deleteMany({ where: { id: testBlog1.id } }));
+  expect(await fetchBlog1()).toBeNull();
+  expect(await fetchBlog2()).toBeTruthy();
 });
