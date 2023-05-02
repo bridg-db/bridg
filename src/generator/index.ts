98312594175dc85b50b87f86d59436d14763d85e
Joe Roddy <mrjoeroddy@gmail.com>
2023-05-02 19:25:21 -0400

    log cli args

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -9,6 +9,9 @@ const getArgValue = (arg: string, args: string[]) => {
 const [pathToSchema, ...args] = process.argv.slice(2);
 const outputLocation = getArgValue('outdir', args) || './node_modules/bridg/tmp';
 const apiLocation = getArgValue('api', args) || '/api/bridg';
+console.log('path to schema: ', pathToSchema);
+console.log('output location: ', outputLocation);
+console.log('api location: ', apiLocation);
 
 if (!pathToSchema) throw new Error(`Error, schema not provided`);
 generateFilesFromSchemaPath(pathToSchema, outputLocation, apiLocation);
