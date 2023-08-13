cf7f4d026d5168a0bcd2c34781a37a86b40f2750
Joe Roddy <mrjoeroddy@gmail.com>
2023-08-13 13:30:08 -0400

    clean repository

--- src/playground/nextjs/tests/utils/reset-db-data.ts
+++
@@ -1,7 +0,0 @@
-import prisma from '../../prisma/db';
-
-export const resetDbData = async () => {
-  await prisma.blog.deleteMany();
-  await prisma.user.deleteMany();
-  await prisma.comment.deleteMany();
-};
--- src/utils/string.util.ts
+++
@@ -1,2 +0,0 @@
-export const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
-export const uncapitalize = (str: string) => str.charAt(0).toLowerCase() + str.slice(1);
